9fcb3f7da01e81b50bf01bc9f22083ae4313eca8
beefchimi <curtdulmage@gmail.com>
2024-07-07 18:13:58 -0400

    :alembic: [App] Render a blob

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import {useState} from 'react';
 
 import {Frame, Header, Main, Footer, SvgWave} from './components';
+import {SvgBlob} from './experiment';
 
 import styles from './App.module.css';
 
@@ -30,6 +31,12 @@ export default function App() {
       <Footer>
         <p>&copy; Curtis Dulmage&trade; &mdash; 2020&ndash;2024</p>
       </Footer>
+
+      <div className={styles.BlobOuter}>
+        <div className={styles.BlobInner}>
+          <SvgBlob size={100} points={3} />
+        </div>
+      </div>
     </Frame>
   );
 }
